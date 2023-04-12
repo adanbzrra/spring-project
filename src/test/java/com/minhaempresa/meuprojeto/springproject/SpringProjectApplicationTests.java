790ae8d2795c5f88cc32e1777f72877f3f3f9b38
adanbzrra <adanbzrra@gmail.com>
2023-04-12 16:40:35 -0300

    Initial commit

---
+++ src/test/java/com/minhaempresa/meuprojeto/springproject/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhaempresa.meuprojeto.springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
